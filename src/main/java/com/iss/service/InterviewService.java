90f36ab194be90e5e70b4aad95a1e4a8d036d595
anweshxx <118844293+anweshxx@users.noreply.github.com>
2026-04-02 15:38:36 +0530

    InterviewService.java

--- src/main/java/com/iss/service/InterviewService.java
+++ src/main/java/com/iss/service/InterviewService.java
@@ -6,7 +6,16 @@ import com.iss.dto.interview.InterviewResponse;
 import java.util.List;
 
 public interface InterviewService {
+
     InterviewResponse scheduleInterview(InterviewRequest request);
+
+    InterviewResponse updateInterview(Long id, InterviewRequest request);
+
     List<InterviewResponse> getAllInterviews();
+
+    List<InterviewResponse> getInterviewsByCandidate(Long candidateId);
+
     InterviewResponse getInterviewById(Long id);
+
+    void deleteInterview(Long id);
 }
