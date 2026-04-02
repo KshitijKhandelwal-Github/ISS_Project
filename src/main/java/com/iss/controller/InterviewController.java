1e615cfc10549397ffc30283caec35dcce86f22a
KshitijKhandelwal-Github <kshitij.khandelwal@gmail.com>
2026-04-02 14:19:46 +0530

    Reverted InterviewController to previous version

--- src/main/java/com/iss/controller/InterviewController.java
+++ src/main/java/com/iss/controller/InterviewController.java
@@ -1,37 +1,37 @@
 package com.iss.controller;
 
-import com.iss.dto.interview.InterviewRequest;
 import com.iss.dto.interview.InterviewResponse;
+import com.iss.dto.interview.InterviewRequest;
 import com.iss.service.InterviewService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/api/interviews")
-public class InterviewController {
-
-    private final InterviewService interviewService;
-
-    public InterviewController(InterviewService interviewService) {
-        this.interviewService = interviewService;
-    }
-
-    @PostMapping
-    public ResponseEntity<InterviewResponse> scheduleInterview(@RequestBody InterviewRequest request) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(interviewService.scheduleInterview(request));
-    }
-
-    @GetMapping
-    public ResponseEntity<List<InterviewResponse>> getAllInterviews() {
-        return ResponseEntity.ok(interviewService.getAllInterviews());
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<InterviewResponse> getInterviewById(@PathVariable Long id) {
-        return ResponseEntity.ok(interviewService.getInterviewById(id));
-    }
 }
 
+@PostMapping
+@PreAuthorize("hasRole('HR')")
+public ResponseEntity<InterviewResponse> scheduleInterview( @RequestBody InterviewRequest request) {
+    return ResponseEntity.status(HttpStatus.CREATED).body(interviewService.scheduleInterview(request));
+}
+
+@GetMapping
+@PreAuthorize("hasAnyRole('HR', 'TECHNICAL_PANEL')")
+public ResponseEntity<List<InterviewResponse>> getAllInterviews() {
+    return ResponseEntity.ok(interviewService.getAllInterviews());
+}
+
+@GetMapping("/{id}")
+@PreAuthorize("hasAnyRole('HR', 'TECHNICAL_PANEL')")
+public ResponseEntity<InterviewResponse> getInterviewById(@PathVariable Long id) {
+    return ResponseEntity.ok(interviewService.getInterviewById(id));
+}
+}
